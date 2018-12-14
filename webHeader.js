81f145b2eaee07c6ef999c56b8cbba5fb1ebaedc
bray <baishakhir@gmail.com>
2018-12-14 15:35:09 -0500

    updating github page

---
+++ webHeader.js
@@ -0,0 +1,33 @@
+document.write("<div class=\"sf_header_wrapper\"> \
+   <div class=\"sf_region3\"> \
+     <div class=\"sf_main_header\">\
+       <div style=\"display:block;word-wrap: break-word;\">\
+     	    <table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"width: 100%;\">\
+              <tbody>\
+              <tr>\
+		        <td rowspan=\"3\">\
+                    <img alt=\"\" src=\"images/header.png\" width=\"100%\" style=\"border-width: 0px; border-style: solid;\">\
+                </td>\
+	          </tr>     \
+              </tbody>\
+            </table>\
+       </div>\
+      </div>  <!-- sf_main_header -->\
+      <div class=\"sf_navigation\">\
+       <div class=\"widget main_nav\">\
+            <h3 class=\"widget_header\">Main Navigation</h3>\
+       <div class=\"widget_content\">\
+       <div style=\"display:block;\">\
+         <ul id=\"Nav1\" class=\"nav\">\
+          <li class=\"active-nav sf_first_nav_item\"><a href=\"./index.html\" target=\"_self\" style=\"color:#373435\" onclick=\"\">Home</a></li>\
+          <li class=\" sf_last_nav_item\"><a href=\"./Organization.html\" target=\"_self\" style=\"color:#373435\" onclick=\"\">Organization</a></li>\
+          <li class=\" sf_last_nav_item\"><a href=\"./Program.html\" target=\"_self\" style=\"color:#373435\" onclick=\"\">Program</a></li>\
+          <li class=\" sf_last_nav_item\"><a href=\"./Venue.html\" target=\"_self\" style=\"color:#373435\" onclick=\"\">Venue</a></li>\
+         </ul>\
+        </div>\
+        </div>\
+        </div>\
+        </div>   <!-- sf_navigation -->\
+        \
+    </div> <!-- sf_region3 -->\
+  </div>");
