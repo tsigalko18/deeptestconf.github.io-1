59e22c2ed2061393f8873b0135c53e99af7e9474
Andrea Stocco <tsigalko18@gmail.com>
2020-11-17 14:31:16 +0100

    2021 edition draft

---
+++ _site/2020/header.js
@@ -0,0 +1,24 @@
+﻿document.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\
+<meta property=\"og:type\" content=\"article\">\
+<meta property=\"og:image\" content=\"./images/shoot.jpg\">\
+<meta property=\"og:site_name\" content=\"rayb\">\
+<meta property=\"og:description\" content=\"Check out http://rayb.info!  \">\
+<style type=\"text/css\">body { margin:0; padding:0; }</style>\
+\
+<script src=\"./jquery.min.js\"></script>  \
+<script src=\"styles/toggle.js\"></script>\
+\
+<link rel=\"stylesheet\" type=\"text/css\" id=\"toggle.css\" href=\"styles/toggle.css\">\
+\
+\
+<script type=\"text/javascript\" src=\"./styles/imageSwap.js\"></script>\
+<script type=\"text/javascript\">		\
+		MM_preloadImages('images/deep-learning.jpg')\
+</script>\
+\
+\
+<link rel=\"stylesheet\" type=\"text/css\" id=\"layoutcontainer.css\" href=\"./styles/layoutcontainer.css\">\
+<link rel=\"stylesheet\" type=\"text/css\" id=\"theme.css\" href=\"./styles/theme.css\">\
+<link rel=\"stylesheet\" type=\"text/css\" id=\"color_1.css\" href=\"./styles/color_1.css\">\
+    \
+<style type=\"text/css\"></style>");
---
+++ _site/webHeader.js
@@ -0,0 +1,32 @@
+document.write("<div class=\"sf_header_wrapper\"> \
+   <div class=\"sf_region3\"> \
+     <div class=\"sf_main_header\">\
+       <div style=\"display:block;word-wrap: break-word;\">\
+     	    <table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"width: 100%;\">\
+              <tbody>\
+              <tr>\
+		        <td>\
+                    <img alt=\"\" src=\"images/header.jpeg\" width=\"100%\" align=\"middle\" style=\"border-width: 0px; border-style: solid;\">\
+            </td>\
+	          </tr>     \
+              </tbody>\
+            </table>\
+       </div>\
+      </div>  <!-- sf_main_header -->\
+      <div class=\"sf_navigation\">\
+       <div class=\"widget main_nav\">\
+            <h3 class=\"widget_header\">Main Navigation</h3>\
+       <div class=\"widget_content\">\
+       <div style=\"display:block;\">\
+         <ul id=\"Nav1\" class=\"nav\">\
+          <li class=\"active-nav sf_first_nav_item\"><a href=\"./index.html\" target=\"_self\"  onclick=\"\">Home</a></li>\
+          <!--<li class=\" sf_last_nav_item\"><a href=\"#Program\" target=\"_self\"  onclick=\"\">Program</a></li>-->\
+          <li class=\" sf_last_nav_item\"><a href=\"#Organizers\" target=\"_self\"  onclick=\"\">Organizers</a></li>\
+         </ul>\
+        </div>\
+        </div>\
+        </div>\
+        </div>   <!-- sf_navigation -->\
+        \
+    </div> <!-- sf_region3 -->\
+  </div>");
